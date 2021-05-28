5f7e059d5349d5a817cda829a46f070bf6698a14
Szymon Osiecki <szymonos@outlook.com>
2021-05-28 09:26:32 +0200

    profile update

--- .devcontainer/src/profile.ps1
+++ .devcontainer/src/profile.ps1
@@ -8,27 +8,33 @@ Install-Module PSReadLine -AllowPrerelease -Force
 code $Profile.CurrentUserAllHosts
 #>
 # make PowerShell console Unicode (UTF-8) aware
-$OutputEncoding = [Console]::InputEncoding = [Console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+$OutputEncoding = [Console]::InputEncoding = [Console]::OutputEncoding = [Text.UTF8Encoding]::new()
+
 # set variable for Startup Working Directory
 $SWD = $PWD.Path
+
 # enable predictive suggestion feature in PSReadLine
-try { Set-PSReadLineOption -PredictionSource History } catch {}
+try {
+    Set-PSReadLineOption -PredictionSource History
+    Set-PSReadLineOption -PredictionViewStyle ListView
+} catch {}
+
 function Prompt {
     $execStatus = $?
     # format execution time of the last command
     $executionTime = if ((Get-History).Count -gt 0) {
         switch ((Get-History)[-1].Duration) {
-            { $_.TotalMilliseconds -lt 10 } { "{0:N3} ms" -f $_.TotalMilliseconds }
-            { $_.TotalMilliseconds -ge 10 -and $_.TotalMilliseconds -lt 100 } { "{0:N2} ms" -f $_.TotalMilliseconds }
-            { $_.TotalMilliseconds -ge 100 -and $_.TotalMilliseconds -lt 1000 } { "{0:N1} ms" -f $_.TotalMilliseconds }
-            { $_.TotalSeconds -ge 1 -and $_.TotalSeconds -lt 10 } { "{0:N3} s" -f $_.TotalSeconds }
-            { $_.TotalSeconds -ge 10 -and $_.TotalSeconds -lt 100 } { "{0:N2} s" -f $_.TotalSeconds }
+            { $_.TotalMilliseconds -lt 10 } { '{0:N3} ms' -f $_.TotalMilliseconds }
+            { $_.TotalMilliseconds -ge 10 -and $_.TotalMilliseconds -lt 100 } { '{0:N2} ms' -f $_.TotalMilliseconds }
+            { $_.TotalMilliseconds -ge 100 -and $_.TotalMilliseconds -lt 1000 } { '{0:N1} ms' -f $_.TotalMilliseconds }
+            { $_.TotalSeconds -ge 1 -and $_.TotalSeconds -lt 10 } { '{0:N3} s' -f $_.TotalSeconds }
+            { $_.TotalSeconds -ge 10 -and $_.TotalSeconds -lt 100 } { '{0:N2} s' -f $_.TotalSeconds }
             { $_.TotalSeconds -ge 100 -and $_.TotalHours -le 1 } { $_.ToString('mm\:ss\.ff') }
             { $_.TotalHours -ge 1 -and $_.TotalDays -le 1 } { $_.ToString('hh\:mm\:ss') }
             { $_.TotalDays -ge 1 } { "$($_.Days * 24 + $_.Hours):$($_.ToString('mm\:ss'))" }
         }
     } else {
-        "0 ms"
+        '0 ms'
     }
     # set prompt path
     $promptPath = $PWD.Path.Replace($HOME, '~').Replace('Microsoft.PowerShell.Core\FileSystem::', '') -replace '\\$', ''
@@ -42,10 +48,10 @@ function Prompt {
     [Console]::Write("`u{279C} `e[1m`e[34m{0}", $promptPath)
     try {
         # show git branch name
-        if ($gstatus = [string[]]@(git status -b --porcelain=v1 2>$null)[0..1]) {
+        if ($gstatus = @(git status -b --porcelain=v1 2>$null)[0..1]) {
             [Console]::Write(" `e[96m(")
             # parse branch name
-            if ($gstatus[0] -like '## No commits yet*') {
+            if ($gstatus[0] -match '^## No commits yet') {
                 $branch = $gstatus[0].Split(' ')[5]
             } else {
                 $branch = $gstatus[0].Split(' ')[1].Split('.')[0]
@@ -57,26 +63,93 @@ function Prompt {
     } catch {}
     return "`e[0m{0} " -f ('>' * ($nestedPromptLevel + 1))
 }
+
 function Get-CmdletAlias ($cmdletname) {
     <#.SYNOPSIS
     Gets the aliases for any cmdlet.#>
     Get-Alias |
-    Where-Object -FilterScript { $_.Definition -like "*$cmdletname*" } |
-    Sort-Object -Property Definition, Name |
-    Select-Object -Property Definition, Name
+        Where-Object -FilterScript { $_.Definition -match $cmdletname } |
+        Sort-Object -Property Definition, Name |
+        Select-Object -Property Definition, Name
 }
+
 function Get-CommandSource ($cmdname) {
     <#.SYNOPSIS
     Gets the source directory for command.#>
     (Get-Command $cmdname).Source
 }
 Set-Alias -Name which -Value Get-CommandSource
+
 function Set-StartupLocation {
     <#.SYNOPSIS
     Sets the current working location to the startup working directory.#>
     Set-Location $SWD
 }
 Set-Alias -Name cds -Value Set-StartupLocation
+
+function Get-DiskUsage {
+    [cmdletbinding()]
+    param (
+        [Alias('p')][Parameter(Position = 0)][string]$Path = '*',
+        [Alias('h')][switch]$HumanReadable,
+        [Alias('r')][switch]$Recurse,
+        [Alias('a')][switch]$All,  # include hidden files and folders
+        [Alias('s')][ValidateSet('size', 'count','name')][string]$Sort
+    )
+    <#.SYNOPSIS
+    Gets summary size of files inside folders.#>
+    # filter for size formatting
+    filter formatSize {
+        switch ($_) {
+            { $_ -ge 1KB -and $_ -lt 1MB } { '{0:0.0}K' -f ($_ / 1KB) }
+            { $_ -ge 1MB -and $_ -lt 1GB } { '{0:0.0}M' -f ($_ / 1MB) }
+            { $_ -ge 1GB -and $_ -lt 1TB } { '{0:0.0}G' -f ($_ / 1GB) }
+            { $_ -ge 1TB } { '{0:0.0}T' -f ($_ / 1TB) }
+            Default { "$_.0B" }
+        }
+    }
+    if ($Recurse) {
+        $Path = $Path.Replace('*', '.')
+        $cmd = "Get-ChildItem '$Path' -Recurse -Directory$($All ? ' -Force' : '')"
+    } else {
+        $cmd = "Get-Item '$Path'$($All ? ' -Force' : '') | Where-Object { `$_.PSIsContainer }"
+    }
+    [Collections.Generic.List[string]]$dirs = (Invoke-Expression $cmd).FullName
+    if ($Recurse) { $dirs.Add((Resolve-Path $Path).Path) }
+    if ($Sort) { $result = [Collections.Generic.List[PSObject]]::new() }
+    foreach ($dir in $dirs) {
+        $cmd = "Get-ChildItem '$dir' -File$($All ? ' -Force' : '')$($Recurse ? '' : ' -Recurse')"
+        $items = Invoke-Expression $cmd
+        $size = 0 + ($items | Measure-Object -Property Length -Sum).Sum
+        $cnt = $items.Count
+        $relPath = Resolve-Path $dir -Relative
+        if ($Sort) {
+            $result.Add([PSCustomObject]@{
+                    Size  = $size
+                    Count = $cnt
+                    Name  = $relPath
+                })
+        } else {
+            if ($HumanReadable) {
+                $size = $size | formatSize
+                "$(' ' * (6 - $size.Length))$size   $(' ' * (8 - $cnt.ToString().Length))$cnt   $relPath"
+            } else {
+                "$(' ' * (16 - $size.ToString().Length))$size   $(' ' * (8 - $cnt.ToString().Length))$cnt   $relPath"
+            }
+        }
+    }
+    if ($Sort) {
+        $result | Sort-Object -Property $Sort | Format-Table -HideTableHeaders @{Name = 'Size'; Expression = { $HumanReadable ? ($_.Size | formatSize) : ($_.Size) }; Align = 'Right' }, Count, Name
+    }
+}
+Set-Alias -Name du -Value Get-DiskUsage
+
+# activate python virtual environment if exists
+$init = [IO.Path]::Combine('.vscode', 'init.ps1')
+if (Test-Path $init) { & $init }
+if ($IsLinux) {
+    Set-Alias -Name '.venv/bin/activate' -Value '.venv/bin/Activate.ps1'
+}
 
 # PowerShell startup information
 Clear-Host
